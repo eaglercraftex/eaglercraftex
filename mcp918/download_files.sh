75ac2f7235f7b26108dfe67969e62724e031b4e4
theseandiddycombs <thewordiseasy@gmail.com>
2025-07-24 09:38:51 +0000

    Update download_files.sh
    curl is installed by default on macos and linux

--- mcp918/download_files.sh
+++ mcp918/download_files.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
-set -e
 
-if command -v curl >/dev/null 2>&1; then
-  DL_CMD="curl -fL -o"
-elif command -v wget >/dev/null 2>&1; then
-  DL_CMD="wget -O"
-else
-  echo "Neither curl nor wget is installed. Please install one and try again."
-  exit 1
-fi
-
-$DL_CMD client.jar https://piston-data.mojang.com/v1/objects/0983f08be6a4e624f5d85689d1aca869ed99c738/client.jar
-$DL_CMD 1.8.json https://launchermeta.mojang.com/v1/packages/f6ad102bcaa53b1a58358f16e376d548d44933ec/1.8.json
-$DL_CMD mcp918.zip https://github.com/leijurv/MineBot/raw/refs/heads/master/mcp918.zip
+curl -f -L --output client.jar https://piston-data.mojang.com/v1/objects/0983f08be6a4e624f5d85689d1aca869ed99c738/client.jar
+curl -f -L --output 1.8.json https://launchermeta.mojang.com/v1/packages/f6ad102bcaa53b1a58358f16e376d548d44933ec/1.8.json
+curl -f -L --output mcp918.zip https://github.com/leijurv/MineBot/raw/refs/heads/master/mcp918.zip
 
 echo "All files downloaded successfully."
